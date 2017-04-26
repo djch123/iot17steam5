773ce69496f3f1c7bc452e2b825600fb32158c76
jialingliu <jialing.whu@gmail.com>
2017-04-25 20:11:04 -0700

    android initial framework

---
+++ EmotionPlus/app/src/main/java/com/example/jialingliu/emotionplus/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.jialingliu.emotionplus;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_main:
+                    mTextMessage.setText(R.string.title_main);
+                    return true;
+                case R.id.navigation_homekit:
+                    mTextMessage.setText(R.string.title_homekit);
+                    return true;
+                case R.id.navigation_recommendation:
+                    mTextMessage.setText(R.string.title_recommendation);
+                    return true;
+                case R.id.navigation_game:
+                    mTextMessage.setText(R.string.title_game);
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
