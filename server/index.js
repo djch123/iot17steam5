d124bec6391c76f80d03df29f90fb2c0ab6d77b8
Changda Li <lichangdazju@yeah.net>
2017-04-28 22:30:26 -0700

    modified rest api

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
-app.use('/music', require('./routers/music'));
+
+app.use(bodyParser.json());
+
+app.use('/recommendation', require('./routers/recommendation'));
+
 
 app.listen(3000, function() {
 	console.log('listening on port: 3000');
---
+++ server/routers/music.js
@@ -0,0 +1,31 @@
+var express = require('express')
+  , router = express.Router()
+  , player = require('../lib/music_player')
+  , list = require('../music/list');
+
+var index = 0;
+
+
+router.get('/', function() {
+	player.play(__dirname + '/../music/Cloud/' + list.happy[index]);
+});
+
+router.get('/pause', function() {
+	player.pause();
+});
+
+router.get('/resume', function() {
+	player.resume();
+});
+
+router.get('/stop', function() {
+	player.stop();
+});
+
+router.get('/next', function(req, res) {
+	index = index + 1 >= list.happy.length ?  0 : index + 1;
+	res.send(index.toString());
+	player.play(__dirname + '/../music/Cloud/' + list.happy[index]);
+});
+
+module.exports = router;
