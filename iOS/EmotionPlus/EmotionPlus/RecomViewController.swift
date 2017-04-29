06c34ad75443ac8749b1a1e578f8866b5bb2e5e0
Scott Wang <xiaozhe.wang@sv.cmu.edu>
2017-04-28 20:57:10 -0700

    ios frame

---
+++ iOS/EmotionPlus/EmotionPlus/RecomViewController.swift
@@ -0,0 +1,24 @@
+//
+//  RecomViewController.swift
+//  EmotionPlus
+//
+//  Created by Xiaozhe Wang on 4/28/17.
+//  Copyright © 2017 iot17steam5. All rights reserved.
+//
+
+import UIKit
+
+class RecomViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
