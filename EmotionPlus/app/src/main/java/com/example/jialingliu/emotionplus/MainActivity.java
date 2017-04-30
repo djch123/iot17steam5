6d93c08c4195ead95a7db4dfa9ed947882379637
sytedian <sytedian@sytediandeMacBook-Pro.local>
2017-04-29 19:51:12 -0700

    add hue

--- EmotionPlus/app/src/main/java/com/example/jialingliu/emotionplus/MainActivity.java
+++ EmotionPlus/app/src/main/java/com/example/jialingliu/emotionplus/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
