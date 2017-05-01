7d861e1b2c2db6e4c63cb69f08abce711c60000e
sytedian <sytedian@sytediandeMacBook-Pro.local>
2017-04-30 20:13:49 -0700

    finishe music

--- EmotionPlus/app/src/main/java/com/example/jialingliu/emotionplus/HomekitActivity.java
+++ EmotionPlus/app/src/main/java/com/example/jialingliu/emotionplus/HomekitActivity.java
@@ -1,14 +1,21 @@
 package com.example.jialingliu.emotionplus;
 
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.view.View.OnClickListener;
 import android.util.Log;
 import android.view.View;
 
+
+import com.loopj.android.http.*;
 import com.philips.lighting.hue.sdk.PHHueSDK;
 import com.philips.lighting.model.PHBridge;
 import com.philips.lighting.model.PHLight;
@@ -17,9 +24,18 @@ import com.philips.lighting.hue.listener.PHLightListener;
 import com.philips.lighting.model.PHBridgeResource;
 import com.philips.lighting.model.PHHueError;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
+import cz.msebera.android.httpclient.Header;
+import cz.msebera.android.httpclient.entity.StringEntity;
 
 
 /**
@@ -35,7 +51,8 @@ public class HomekitActivity extends AppCompatActivity {
     private Thread brightControllThread = null;
     private int brightThreadState = 0;
     private int index = 0;
-    private List<String> songs = new ArrayList<>();
+    private JSONArray songs = new JSONArray();
+    private boolean play = true;
 
 
     @Override
@@ -45,6 +62,9 @@ public class HomekitActivity extends AppCompatActivity {
         //textView = (TextView) findViewById(R.id.helloworld1);
         // textView.setText("Here is the homekit");
         phHueSDK = PHHueSDK.create();
+
+
+
         findViewById(R.id.buttonOn).setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -58,11 +78,32 @@ public class HomekitActivity extends AppCompatActivity {
             }
         });
 
+        findViewById(R.id.playSong).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(play==true) {
+                    findViewById(R.id.playSong).setBackgroundResource(R.drawable.icon_musicplay);
+                    play=false;
+                    pauseMusic();
+                }else{
+                    findViewById(R.id.playSong).setBackgroundResource(R.drawable.icon_musicstop);
+                    play=true;
+                    resumeMusic();
+                }
+            }
+        });
+
+
         findViewById(R.id.previousSong).setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(index>0){
                     index--;
+                    try{
+                        playMusic();
+                    }catch(Exception e){
+
+                    }
                 }
             }
         });
@@ -70,29 +111,207 @@ public class HomekitActivity extends AppCompatActivity {
         findViewById(R.id.nextSong).setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(index<songs.size()-1){
+                if(index<songs.length()-1){
                     index++;
+                    try{
+                        playMusic();
+                    }catch(Exception e){
+
+                    }
                 }
             }
         });
+
+        try {
+            getEmotion();
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
-    public void getEmotion(){
-        stopThread();
+    public void getEmotion() throws JSONException{
+        //stopThread();
+        HttpUtils.get("http://172.29.92.105:8888/emotion", null, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                // If the response is JSONObject instead of expected JSONArray
+                Log.i("taghere","response"+response);
+                try {
+                    sendEmotion(response);
+                }catch(Exception e){
+
+                }
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray timeline) {
+                // Pull out the first event on the public timeline
+                //JSONObject firstEvent = timeline.get(0);
+                //String tweetText = firstEvent.getString("text");
+
+                // Do something with the response
+                System.out.println(timeline);
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String x, Throwable e){
+                Log.i("taghere1","failure");
+            }
+
+        });
+    }
+
+    public void sendEmotion(JSONObject response) throws Exception{
+
+        StringEntity entity = new StringEntity(response.toString());
+
+
+        HttpUtils.post(null,"http://172.29.93.218:3000/recommendation/music", entity, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                // If the response is JSONObject instead of expected JSONArray
+                Log.i("taghere","response"+response);
+
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray timeline) {
+                // Pull out the first event on the public timeline
+                //JSONObject firstEvent = timeline.get(0);
+                //String tweetText = firstEvent.getString("text");
+
+                // Do something with the response
+                System.out.println(timeline);
+                songs=timeline;
+                index=0;
+                try {
+                    playMusic();
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String x, Throwable e){
+                Log.i("taghere1","failure");
+            }
+
+        });
+    }
+
+
+
+    public void playMusic() throws Exception{
+
+        JSONObject song = (JSONObject)songs.get(index);
+        String name = song.get("name").toString();
+        String singer = song.get("singer").toString();
+        ((TextView)findViewById(R.id.textViewSong)).setText(name);
+        ((TextView)findViewById(R.id.textViewSinger)).setText(singer);
+        String picURL =song.get("img").toString();
+        //URL url = new URL("http://"+picURL);
+        //Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+        //Bitmap bmp = BitmapFactory.decodeStream((InputStream)new URL("http://"+picURL).getContent());
+        //BitmapDrawable background = new BitmapDrawable(bmp);
+        //((ImageView)findViewById(R.id.cover)).setImageBitmap(bmp);
+        new DownloadImageTask((ImageView) findViewById(R.id.cover))
+                .execute("http://"+picURL);
+        //findViewById(R.id.cover).setBackground(background);
+        play=true;
+        findViewById(R.id.playSong).setBackgroundResource(R.drawable.icon_musicstop);
+
+        Log.i("name",name);
+        HttpUtils.get("http://172.29.93.218:8080/music?song="+name, null, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                // If the response is JSONObject instead of expected JSONArray
+                Log.i("taghere","response"+response);
+
+
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray timeline) {
+                // Pull out the first event on the public timeline
+                //JSONObject firstEvent = timeline.get(0);
+                //String tweetText = firstEvent.getString("text");
+
+                // Do something with the response
+                //findViewById(R.id.playSong).setBackgroundResource(R.drawable.icon_musicplay);
+
+                System.out.println(timeline);
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String x, Throwable e){
+                Log.i("taghere1","failure");
+            }
+
+        });
+
 
     }
 
-    public List<String> getSongs(){
-        return new ArrayList<>();
+    public void pauseMusic(){
+        HttpUtils.get("http://172.29.93.218:8080/music/pause", null, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                // If the response is JSONObject instead of expected JSONArray
+                Log.i("taghere","response"+response);
+                findViewById(R.id.playSong).setBackgroundResource(R.drawable.icon_musicplay);
+                play=false;
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray timeline) {
+                // Pull out the first event on the public timeline
+                //JSONObject firstEvent = timeline.get(0);
+                //String tweetText = firstEvent.getString("text");
+
+                // Do something with the response
+                findViewById(R.id.playSong).setBackgroundResource(R.drawable.icon_musicplay);
+                play=false;
+                System.out.println(timeline);
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String x, Throwable e){
+                Log.i("taghere1","failure");
+            }
+
+        });
+
     }
 
-    public void playMusic(){
+    public void resumeMusic(){
+        HttpUtils.get("http://172.29.93.218:8080/music/resume", null, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                // If the response is JSONObject instead of expected JSONArray
+                Log.i("taghere","response"+response);
+                findViewById(R.id.playSong).setBackgroundResource(R.drawable.icon_musicstop);
+                play=true;
+
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray timeline) {
+                // Pull out the first event on the public timeline
+                //JSONObject firstEvent = timeline.get(0);
+                //String tweetText = firstEvent.getString("text");
+
+                // Do something with the response
+                findViewById(R.id.playSong).setBackgroundResource(R.drawable.icon_musicstop);
+                play=true;
+                System.out.println(timeline);
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String x, Throwable e){
+                Log.i("taghere1","failure");
+            }
+
+        });
 
     }
 
-    public void stopMusic(){
-
-    }
     public void turnOn() {
         stopThread();
         PHBridge bridge = phHueSDK.getSelectedBridge();
@@ -173,4 +392,28 @@ public class HomekitActivity extends AppCompatActivity {
         }
     }
 
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
+    }
 }
