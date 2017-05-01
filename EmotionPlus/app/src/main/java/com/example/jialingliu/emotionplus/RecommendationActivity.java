74509b67180ac41ef3d58be69a75b57b8c1992a2
jialingliu <jialing.whu@gmail.com>
2017-05-01 12:51:41 -0700

    modified recommendations

--- EmotionPlus/app/src/main/java/com/example/jialingliu/emotionplus/RecommendationActivity.java
+++ EmotionPlus/app/src/main/java/com/example/jialingliu/emotionplus/RecommendationActivity.java
@@ -5,7 +5,6 @@ import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
