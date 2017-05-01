633364d3034c5d41ec0cddec8aab97a4bf465814
Scott Wang <xiaozhe.wang@sv.cmu.edu>
2017-04-30 21:50:23 -0700

    history page

---
+++ iOS/EmotionPlus/HistoryTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  HistoryTableViewCell.swift
+//  EmotionPlus
+//
+//  Created by Xiaozhe Wang on 4/30/17.
+//  Copyright © 2017 iot17steam5. All rights reserved.
+//
+
+import UIKit
+
+class HistoryTableViewCell: UITableViewCell {
+
+    @IBOutlet var label: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
