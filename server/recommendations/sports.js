5effd1a431bb818579626465cdad43eac1a37ec9
Changda Li <lichangdazju@yeah.net>
2017-05-01 20:08:40 -0700

    changed recommendation number to 2

--- server/recommendations/sports.js
+++ server/recommendations/sports.js
@@ -1,6 +1,15 @@
 module.exports = 
 {
-	"happiness" : [{"name":"ping pong","img":"ping pong.jpg"}],
-	"angry": [{"name":"boxing","img":"/sports/boxing.jpg"}],
-	"sad": [{"name":"jogging","img":"/sports/jogging.jpg"}]
+"happiness" : [
+	{"name":"ping pong","img":"/sports/ping pong.jpg"},
+	{"name":"tennis","img":"/sports/tennis.jpg"}
+	],
+"angry": [
+	{"name":"boxing","img":"/sports/boxing.jpg"},
+	{"name":"football","img":"/sports/football.jpg"}
+	],
+"sad": [
+	{"name":"jogging","img":"/sports/jogging.jpg"},
+	{"name":"tai-chi","img":"/sports/tai-chi.jpg"}
+	]
 };
