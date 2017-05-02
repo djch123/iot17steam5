be497694500eecd2569f2b705e4eb2627c36d004
callMeName <rickyxuusa@gmail.com>
2017-05-02 00:28:02 -0700

    test

--- raspberrypi/server/app.py
+++ raspberrypi/server/app.py
@@ -5,6 +5,8 @@ from capture import capture
 import requests
 from random import randint
 import os.path, time
+import subprocess
+
 
 app = Flask(__name__, static_url_path="", static_folder="./")
 
@@ -109,49 +111,53 @@ def stop_motion():
 
 @app.route("/captureinstream")
 def captureinstream():
-	# try:
-	# 	r = requests.get("http://" + conf['pi_ip'] + ":8080/0/action/snapshot")
-	# 	time.sleep(5)
-	# 	r.raise_for_status()
-	# except Exception as e:
-	# 	print str(e)
-	# 	return str(e), 500
-
-	# try:
-
-	# 	anaylze_url = "http://" + conf['anaylze_ip'] + ":" + conf['anaylze_port'] + "/analyze"
-	# 	image = open(conf["stream_snap_path"])
-	# 	data = image.read()
-	# 	image.close()
-	# 	res = requests.post(url=anaylze_url,
-	# 			data=data,
-	# 			headers={'Content-Type': 'application/octet-stream'})
-		
-	# 	res.raise_for_status()
-
-	# 	j = res.json()
-	# 	return render_template('snap.html', ip=conf['pi_ip'], port=str(conf['pi_port']), data=j)
-	# except requests.exceptions.HTTPError as e:
-	# 	print str(e)
-	# 	return render_template('snap.html', ip=conf['pi_ip'], port=str(conf['pi_port']), error="Can't find a face:)")
-	# except Exception as e:
-	# 	print str(e)
-	# 	return str(e), 500
-
-
-	
 	try:
-		stop_motion()
+		r = requests.get("http://" + conf['pi_ip'] + ":8080/0/action/snapshot")
+		r.raise_for_status()
+	except Exception as e:
+		print str(e)
+		return str(e), 500
 
-		time.sleep(10)
-		capture_helper()
-		return render_template('snap.html', ip=conf['pi_ip'], port=str(conf['pi_port']), data=json.dumps(cur_emotion))
+	try:
+
+		anaylze_url = "http://" + conf['anaylze_ip'] + ":" + conf['anaylze_port'] + "/analyze"
+		# image = open(conf["stream_snap_path"])
+		image = open(conf["image_path"])
+		
+		data = image.read()
+		image.close()
+		res = requests.post(url=anaylze_url,
+				data=data,
+				headers={'Content-Type': 'application/octet-stream'})
+		
+		os.system("mv " + conf['stream_snap_path'] + " " + os.path.dirname(__file__) +  "/image.jpg")
+
+		res.raise_for_status()
+
+
+		j = res.json()
+		return render_template('snap.html', ip=conf['pi_ip'], port=str(conf['pi_port']), data=j)
 	except requests.exceptions.HTTPError as e:
 		print str(e)
 		return render_template('snap.html', ip=conf['pi_ip'], port=str(conf['pi_port']), error="Can't find a face:)")
 	except Exception as e:
 		print str(e)
 		return str(e), 500
+
+
+	
+	# try:
+	# 	stop_motion()
+
+	# 	time.sleep(10)
+	# 	capture_helper()
+	# 	return render_template('snap.html', ip=conf['pi_ip'], port=str(conf['pi_port']), data=json.dumps(cur_emotion))
+	# except requests.exceptions.HTTPError as e:
+	# 	print str(e)
+	# 	return render_template('snap.html', ip=conf['pi_ip'], port=str(conf['pi_port']), error="Can't find a face:)")
+	# except Exception as e:
+	# 	print str(e)
+	# 	return str(e), 500
 		
 
 
