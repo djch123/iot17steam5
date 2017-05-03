c79d474b1a58ca8654caaeea0a6709d8c16a3744
jialingliu <jialing.whu@gmail.com>
2017-05-03 15:39:01 -0700

    hardcoded IP

--- EmotionPlus/app/src/main/java/com/example/jialingliu/emotionplus/Constant.java
+++ EmotionPlus/app/src/main/java/com/example/jialingliu/emotionplus/Constant.java
@@ -4,7 +4,9 @@ package com.example.jialingliu.emotionplus;
  * @author jialingliu
  */
 
-class Constant {
+public class Constant {
     static String PI_IP = "172.29.92.105";
     static String RECOMMENDATION_IP = "172.29.93.218";
+    public static String HUE_IP = "10.0.14.6";
+    public static String HUE_USERNAME = "-rJnC9YCU0BdyWcpaJoHV4E9Yv3pL2hWPxQHbPPq";
 }
